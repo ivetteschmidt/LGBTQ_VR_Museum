1a5fc8d40f39dcc871a5cd32372bc7642f57351d
Sam Roach <sam.roach@unity3d.com>
2021-04-16 14:38:56 +0100

    scripts in folder. fade in texture on ovrmanager

---
+++ VRLiDARFromScratch/Assets/Scripts/PlayerInit.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+// This is the script which will either enable gaze movement or controllers.
+public class PlayerInit : MonoBehaviour
+{
+    public GameObject WhiteSphere; 
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
