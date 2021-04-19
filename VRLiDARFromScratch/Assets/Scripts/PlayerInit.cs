93a03c6344feab7d2547904c4ac412156be2d04d
Sam Roach <sam.roach@unity3d.com>
2021-04-19 16:32:24 +0100

    Option for free movement or teleportation for comfort. Need to add ui, enable with boolean for now

--- VRLiDARFromScratch/Assets/Scripts/PlayerInit.cs
+++ VRLiDARFromScratch/Assets/Scripts/PlayerInit.cs
@@ -6,12 +6,27 @@ using UnityEngine;
 // This is the script which will either enable gaze movement or controllers.
 public class PlayerInit : MonoBehaviour
 {
-    public GameObject WhiteSphere; 
+    [SerializeField] private OVRPlayerController playrecontroller;
+    [SerializeField] private GameObject locomotion;
+    [SerializeField] private CharacterController charController;
+    public bool Freemovement;
+
 
     // Start is called before the first frame update
-    void Start()
+    void Awake()
     {
-        
+        if (Freemovement == true)
+        {
+            playrecontroller.enabled = true;
+            charController.enabled = true;
+            locomotion.SetActive(false);
+        }
+        else if (Freemovement == false) // teleportation enabled
+        {
+            charController.enabled = false;
+            playrecontroller.enabled = false;
+            locomotion.SetActive(true);
+        }
     }
 
     // Update is called once per frame
