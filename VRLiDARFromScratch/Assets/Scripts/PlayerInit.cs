7b4c89653b01de8052c60cc731882853b59f8895
ivette <ivette@therabytes.de>
2021-04-28 21:38:30 +0200

    fps controller without hmd
    unity automagically chooses right player controller now.
    still missing: button pressing

--- VRLiDARFromScratch/Assets/Scripts/PlayerInit.cs
+++ VRLiDARFromScratch/Assets/Scripts/PlayerInit.cs
@@ -1,37 +1,50 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
 
 // This is the script which will either enable gaze movement or controllers.
 public class PlayerInit : MonoBehaviour
 {
-    [SerializeField] private OVRPlayerController playrecontroller;
+    [SerializeField] private OVRPlayerController ovrPlayerController;
+    [SerializeField] private GameObject fpsController;
     [SerializeField] private GameObject locomotion;
     [SerializeField] private CharacterController charController;
     public bool Freemovement;
 
-
-    // Start is called before the first frame update
-    void Awake()
+    bool oculusPresent = false;
+    void CheckOculusPresence()
     {
+        oculusPresent = OVRManager.isHmdPresent;
+    }
+
+    void Start()
+    {
+        CheckOculusPresence();
+        OVRManager.HMDAcquired += CheckOculusPresence;
+        OVRManager.HMDLost += CheckOculusPresence;
+
+        if (!oculusPresent)
+        {
+            fpsController.SetActive(true);
+            ovrPlayerController.gameObject.SetActive(false);
+            return;
+        }
+
+        //if oculus present
+        fpsController.SetActive(false);
+        ovrPlayerController.gameObject.SetActive(true);
+
         if (Freemovement == true)
         {
-            playrecontroller.enabled = true;
+            ovrPlayerController.enabled = true;
             charController.enabled = true;
             locomotion.SetActive(false);
         }
         else if (Freemovement == false) // teleportation enabled
         {
             charController.enabled = false;
-            playrecontroller.enabled = false;
+            ovrPlayerController.enabled = false;
             locomotion.SetActive(true);
         }
-    }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
     }
 }
